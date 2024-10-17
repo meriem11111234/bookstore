3e45e15ed30f218ab8551cdc4b593e89826ed9ee
meriem11111234 <168181611+meriem11111234@users.noreply.github.com>
2024-10-17 11:47:23 +0200

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://meriembaha2611:Z6SYikVAc3RNpst@cluster0.oeu2gfe.mongodb.net/';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
